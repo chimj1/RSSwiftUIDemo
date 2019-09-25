36da481809d4f5448ce57ae32215d90a9f954c2b
chimj <442656651@qq.com>
2019-09-25 10:18:03 +0800

    Initial Commit

---
+++ RSSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RSSwiftUIDemo
+//
+//  Created by backbone on 2019/9/25.
+//  Copyright © 2019 RedSailfish. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
